31cd6a39ea252499db179e1efa34e8894925d4ef
borzecho <Gogany123$>
2023-06-06 15:31:12 +0200

    Initial commit

---
+++ src/main/java/com/borzechowski/mvnjenkins/MvnjenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.borzechowski.mvnjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MvnjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnjenkinsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String get(){
+		return "Hello Jenkins";
+	}
+}
